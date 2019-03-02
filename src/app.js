036131c1ce35fbae8892f5fcb230f54a5f79aff2
KevinMidboe <kevin.midboe@gmail.com>
2019-03-02 22:27:39 +0100

    Defined correct cors requirements for options and Access-Control-Allow-Origin to all requestes.

--- src/app.js
+++ src/app.js
@@ -3,9 +3,22 @@ const logger = require('morgan')
 const bodyParser = require('body-parser')
 
 const app = express();
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.options("/*", function(req, res, next){
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.send(200);
+});
+
+app.all('/*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+  next();
+})
 
 require('./routes')(app);
 

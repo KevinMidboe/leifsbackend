c7cd82af5fd1f3174e87cd93e3a47ae9b92fb880
KevinMidboe <kevin.midboe@gmail.com>
2019-03-03 22:06:50 +0100

    Pulled the origin out in a variable.

--- src/app.js
+++ src/app.js
@@ -3,25 +3,27 @@ const logger = require('morgan')
 const bodyParser = require('body-parser')
 
 const routes = require('./routes')
-
 const app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-routes(app)
+
+const ORIGIN = 'http://localhost:8080';
 
 app.options("/*", function(req, res, next){
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Origin', ORIGIN);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   res.send(200);
 });
 
 app.all('/*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+  res.header('Access-Control-Allow-Origin', ORIGIN)
   next();
 })
+
+routes(app)
 
 app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to the beginning of nothingness',
